2897494e4f527420340f89b0d26b7147ffa167e9
Renan Delarosa <renanlarosa@hotmail.com>
2024-09-16 19:54:16 -0300

    feat: create todo route

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import todo from "./routes/todo";
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.use(todo);
 
--- routes/todo.ts
+++ routes/todo.ts
@@ -1,9 +1,26 @@
 import { Router } from "express";
+import fs from "fs";
 
 const router = Router();
 
+function checkIfFileExist() {
+  try {
+    const buffer = fs.readFileSync("todo.txt");
+    return !!buffer;
+  } catch (error) {
+    return false    
+  }
+}
+
 router.post("/todo", (req, res) => {
-  res.send("Create a todo");
+  const exist = checkIfFileExist();
+  if (!exist) fs.writeFile("todo.txt", "", (err) => {});
+  const description = req.body.description;
+  const randomId = Math.floor(Math.random() * 100000);
+  const done = 0
+  const todo = `${randomId} - ${description} - ${done}`;
+  fs.appendFile("todo.txt", `\n${todo}`, (err) => {});
+  res.send(`Todo '${todo}' created`);
 });
 
 router.get("/todo", (req, res) => {
@@ -14,4 +31,4 @@ router.patch("/todo", (req, res) => {
   res.send("Update a todo");
 });
 
-export default router
+export default router;
